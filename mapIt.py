245e8145ef8016b7bf079c8dad428c0a229bce0d
orrpeles <orr.backup37@gmail.com>
2019-05-15 23:53:10 +0300

    finalized maps script

--- mapIt.py
+++ mapIt.py
@@ -3,6 +3,9 @@
 
 import webbrowser, sys, pyperclip
 if len (sys.argv) > 1: # if I type in cmd line anything after 'python mapIt.py'
-    address = ' '.join(argv[1:])
-    print(address)
+    address = ' '.join(sys.argv[1:])
+else:
+    address = pyperclip.paste()
+webbrowser.open('https://www.google.com/maps/place/' + address)
 
+#print(address)
