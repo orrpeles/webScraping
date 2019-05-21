2cec8d55b84bc87e9404011eef996b3a399d0f2b
orrpeles <orr.backup37@gmail.com>
2019-05-21 21:28:42 +0300

    added selenium examples

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Simple web scraping educational material
+Checkout BS4 branch for the lucky.py program based on beautifulsoup
+checkout selenium for more enhanced educational material on how to take control over your browser
---
+++ selenium/reddit.py
@@ -0,0 +1,17 @@
+#!/bin/python
+# log into reddit from the background
+
+import selenium
+from selenium import webdriver
+import getpass # keep passpord safe
+
+pswd = getpass.getpass('Password:') # reddit password passed to pswd obj
+
+browser = webdriver.Firefox()
+browser.get('https://www.reddit.com/login/?')
+loginElem = browser.find_element_by_id('loginUsername')
+loginElem.send_keys('orrpel')
+passElem = browser.find_element_by_id('loginPassword')
+passElem.send_keys(pswd) #pswd obj called
+passElem.submit()
+
